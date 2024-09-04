133faa749d9e2562708e4c791da1f7447197d3a3
Devika Kumari <devika21@navgurukul.org>
2024-09-05 01:26:38 +0530

    lets start routing page working on mobile view

--- app/assessmentComponents/Pagination.js
+++ app/assessmentComponents/Pagination.js
@@ -6,13 +6,15 @@ function Pagination() {
         <div className='pagination-container'>
             <img className='previous-symbol' src='Component 9.svg' />
             <a className='num1'>1</a>
-            <a className='num2'>2</a>
-            <a className='num2'>3</a>
-            <a className='num2'>4</a>
-            <a className='num2'>5</a>
-            <a className='num2'>6</a>
-            <a className='num2'>7</a>
-            <a className='num2'>8</a>
+            <a className='dote-space'>...</a>
+            <a className='num2 num'>2</a>
+            <a className='num3 num'>3</a>
+            <a className='num4 num'>4</a>
+            <a className='num5 num'>5</a>
+            <a className='dote-space'>...</a>
+            <a className='num6 num'>6</a>
+            <a className='num7 num'>7</a>
+            <a className='num8 num'>8</a>
             <img className='next-symbol' src='Component 10.svg' />
         </div>
     )
